a1b3a90cf138c90372e183aa00754b71b087f45f
maxhasan882 <rhmithu50@gmail.com>
2020-03-22 18:57:01 +0600

    Radium added and used for inline scoped css like hover

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
+import Radium from "radium";
 
 class App extends Component {
   state = {
@@ -44,7 +45,11 @@ class App extends Component {
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black"
+      }
     };
     let persons = null;
     if (this.state.showPerson) {
@@ -64,6 +69,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "#FF1493",
+        color: "black"
+      };
     }
     let classes = [];
     if (this.state.persons.length <= 2) {
@@ -86,4 +95,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
